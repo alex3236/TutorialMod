602753e33e04a9d6c2c502f2607653b7f973e98e
Alex3236 <alex3236@qq.com>
2023-06-21 10:50:02 +0800

    Change log text

--- src/main/java/top/alex3236/TutorialMod.java
+++ src/main/java/top/alex3236/TutorialMod.java
@@ -17,6 +17,6 @@ public class TutorialMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello world!");
 	}
 }
