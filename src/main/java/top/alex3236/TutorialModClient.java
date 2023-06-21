f028e63b2e940f9f00cfa616fc3d2582715502a0
Alex3236 <alex3236@qq.com>
2023-06-21 10:39:36 +0800

    Initial commit

---
+++ src/main/java/top/alex3236/TutorialModClient.java
@@ -0,0 +1,10 @@
+package top.alex3236;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
